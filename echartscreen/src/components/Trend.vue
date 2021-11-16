7bafa4971eccc6822cccca7d4fff6836dbb3dbd0
JMHPC\Administrator <hellojinjin11000@gmal.com>
2021-11-16 22:03:01 +0800

    Revert "折线图完成"
    This reverts commit 29f7d52957ce307fc30cebb29c833c4b847d296e.

--- echartscreen/src/components/Trend.vue
+++ echartscreen/src/components/Trend.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="container">
-    <div class="title" :style="titleFontSize">
-      <div class='text'><span>▎{{title}}</span> <span class="arrow" @click="showSelect = !showSelect">&gt;</span></div>
-      <div class="select" v-show="showSelect">
-        <div class="select-item" v-for="item in selectedTypes" :key="item.key" @click="handleSelect(item.key)">{{item.text}}</div>
-      </div>
-
-    </div>
-
     <div class='main' ref="trend"></div>
   </div>
 </template>
@@ -15,12 +7,12 @@
 <script>
 import chalk from '@/utils/theme.js'
 import { getTrend } from '@/api'
-
+import config from '@/config'
 let colorArr = [
   ['rgb(11,168,44)', 'rgb(11,168,44, 0.5)', 'rgb(11,168,44, 0)'],
-  ['rgb(44,110,255)', 'rgb(44,110,255, 0.5)', 'rgb(44,110,255, 0)'],
-  ['rgb(29,215,194)', 'rgb(29,215,194, 0.5)', 'rgb(29,215,194, 0)'],
-  ['rgb(254,33,30)', 'rgb(254,33,30, 0.5)', 'rgb(254,33,30, 0)'],
+  ['rgb(44,110,255)','rgb(44,110,255, 0.5)', 'rgb(44,110,255, 0)'],
+  ['rgb(29,215,194)', 'rgb(29,215,194, 0.5)','rgb(29,215,194, 0)'],
+  ['rgb(254,33,30)', 'rgb(254,33,30, 0.5)','rgb(254,33,30, 0)'],
   ['rgb(250,105,0)', 'rgb(250,105,0, 0.5)', 'rgb(250,105,0, 0)']
 ]
 export default {
@@ -30,44 +22,38 @@ export default {
       chart: null,
       data: null,
       timer: null,
+      startValue: 0, // datazoom 滑块起始值
+      endValue: 9, // datazoom 滑块结束值
       titleSize: 0, // 标题字体大小
-      selectedType: 'map',
-      showSelect: false,
     }
   },
   mounted() {
     this.initChart()
     this.getData()
-    window.addEventListener('resize', this.adaptChart)
-    this.adaptChart()
-
-
-  },
-  computed: {
-    selectedTypes() {
-      if (!this.data) return []
-      return this.data.type.filter(item => {
-        return item.key != this.selectedType
-      })
-    },
-    title() {
-      if (!this.data) return 'map'
-      return this.data[this.selectedType].title
-    },
-    titleFontSize() {
-      return {
-        fontSize: this.titleSize + 'px'
-      }
-    }
+    // window.addEventListener('resize', this.adaptChart)
+    // this.adaptChart()
+    // this.chart.on('mouseover', 'series', () => {
+    //   clearInterval(this.timer)
+    //   this.timer = null
+    // })
+    // this.chart.on('mouseout', 'series', () => {
+    //   this.startTimer()
+    // })
   },
   destroyed() {
-    window.removeEventListener('resize', this.adaptChart)
+    // clearInterval(this.timer)
+    // window.removeEventListener('resize', this.adaptChart)
   },
   methods: {
     initChart() {
       // 基于准备好的dom，初始化echarts实例
       this.chart = this.$echarts.init(this.$refs.trend, chalk)
       let option = {
+        title: {
+          left: 20,
+          top: 20,
+          text: '▎' + config.title['trend'][0],
+        },
         grid: {
           top: '25%',
           left: '5%',
@@ -75,28 +61,9 @@ export default {
           bottom: '5%',
           containLabel: true
         },
-        legend: {
-          show: true,
-          icon: "circle",
-          textStyle: {
-            color: '#999',
-          }
-        },
+
         tooltip: {
-          show: true,
-          trigger: 'axis',
-          backgroundColor: 'rgba(50,50,50,0.7)',
-          borderWidth: 0,
-          textStyle: {
-            color: '#fff',
-          },
-          formatter: (arg) => {
-            let str = arg[0].axisValue + '<br />'
-            arg.forEach(item => {
-              str += item.marker + item.seriesName + ':' + item.value + '<br />'
-            })
-            return str
-          }
+
         },
         xAxis: {
           type: 'category',
@@ -113,29 +80,35 @@ export default {
     async getData() {
       let res = await getTrend()
       this.data = res.data
+
       this.updateChart()
+      // this.startTimer()
     },
     updateChart() {
-
+      // console.log(this.data);
       let monthData = this.data.common.month
-      let series = this.data[this.selectedType].data.map((item, index) => {
+      let series = this.data.map.data.map((item, index) => {
 
         return {
           name: item.name,
           data: item.data,
           type: "line",
-          stack: this.selectedType,
+          stack: 'map',
           itemStyle: {
             color: colorArr[index][0]
           },
           areaStyle: {
-            color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-              offset: 0,
-              color: colorArr[index][1]
-            }, {
-              offset: 1,
-              color: colorArr[index][2]
-            }]),
+
+            normal: {
+              color: new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                offset: 0,
+                color: colorArr[index][1]
+              }, {
+                offset: 1,
+                color: colorArr[index][2]
+              }]),
+            }
+
           }
         }
       })
@@ -143,36 +116,56 @@ export default {
         xAxis: {
           data: monthData
         },
+        legend: {
+          top: 150,
+          left: 330,
+          show: true,
+          icon: "circle",
+          itemWidth: 33,  // 设置宽度
+          itemHeight: 33, // 设置高度
+          textStyle: {
+            color: '#999',
+            fontSize: 33,
+          }
+        },
         series
       }
       this.chart.setOption(dataOption)
 
     },
+    startTimer() {
+      if (this.timer) clearInterval(this.timer)
+      this.timer = setInterval(() => {
+        this.startValue++
+        this.endValue++
+        if (this.endValue > this.data.length - 1) {
+          this.startValue = 0
+          this.endValue = 9
+        }
+        this.updateChart()
 
+      }, 1500);
+    },
     adaptChart() {
       this.chart.resize()
       this.titleSize = this.$refs.trend.offsetWidth / 100 * 3.6
       let adaptOption = {
-
-        legend: {
-          top: this.titleSize * 3,
-          left: this.titleSize * 5,
-          itemGap: this.titleSize / 2,
-          itemWidth: this.titleSize,  // 设置宽度
-          itemHeight: this.titleSize, // 设置高度
+        title: {
           textStyle: {
-            fontSize: this.titleSize / 1.5,
+            fontSize: this.titleSize
           }
-        }
+        },
+        series: [
+          {
+            barWidth: this.titleSize,
+            itemStyle: {
+              borderRadius: [this.titleSize / 2, this.titleSize / 2, 0, 0],//（顺时针左上，右上，右下，左下）
+            }
+          }
+        ]
       }
       this.chart.setOption(adaptOption)
 
-    },
-
-    handleSelect(type) {
-      this.selectedType = type
-      this.showSelect = false
-      this.updateChart()
     }
 
   }
@@ -181,21 +174,4 @@ export default {
 
 
 <style scoped lang="scss">
-.title {
-  position: absolute;
-  left: 20px;
-  top: 20px;
-  z-index: 2;
-  color: #fff;
-  .text span {
-    float: left;
-  }
-  .arrow {
-    margin-left: 10px;
-    transform: rotate(90deg);
-  }
-  .select {
-    margin-left: 30px;
-  }
-}
 </style>
